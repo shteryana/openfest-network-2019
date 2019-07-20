fa5b07036c6366a5cc2a0925dc6d855a154a970e
Shteryana Shopova <shteryana@gmail.com>
2019-07-20 15:01:19 +0300

    Add liscense

--- access/fetch-keys/main.go
+++ access/fetch-keys/main.go
@@ -1,3 +1,12 @@
+/*-
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 69):
+ * <syrinx@FreeBSD.org> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   -Shteryana Shopova
+ * ----------------------------------------------------------------------------
+ */
+
 package main
 
 import (
